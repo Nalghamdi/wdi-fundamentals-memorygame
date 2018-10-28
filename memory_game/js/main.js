bf4dd9179b174df57c18a15e3e404668a5167ee8
Noni Alghamdi <noni@Nonis-MacBook-Pro.local>
2018-10-28 18:18:58 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,25 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{rank: "queen" ,
+suit: "hearts" ,
+cardImage: "images/queen-of-hearts.png"},
+
+{rank: "queen" ,
+suit: "diamonds" ,
+cardImage: "images/queen-of-diamonds.png"},
+
+{rank: "king" ,
+suit: "hearts" ,
+cardImage: "images/king-of-hearts.png"},
+
+{rank: "king" ,
+suit: "diamonds" ,
+cardImage: "images/king-of-diamonds.png"}
+];
+
+
 var cardsInPlay = [];
 
 
@@ -17,10 +35,13 @@ if (cardsInPlay[0] === cardsInPlay[1])
 
 var flipCard = function (cardId){
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank );
 
+console.log(cards[cardId].cardImage );
 
-cardsInPlay.push(cards[cardId]);
+console.log(cards[cardId].suit );
+
+cardsInPlay.push(cards[cardId].rank);
 
 
 if (cardsInPlay.length === 2)
