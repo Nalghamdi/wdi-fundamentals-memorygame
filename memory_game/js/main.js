37bc00a8426dd9935097358eddb5f5d1bd5050b1
Noni Alghamdi <noni@Nonis-MacBook-Pro.local>
2018-10-30 19:36:00 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,54 @@
-// Unit 7 Assignment (part 6), was'nt saved properly before
+//Ass. part 9, unit 10 
+
+var cards = [
+{rank: "queen" ,
+suit: "hearts" ,
+cardImage: "images/queen-of-hearts.png"},
+
+{rank: "queen" ,
+suit: "diamonds" ,
+cardImage: "images/queen-of-diamonds.png"},
+
+{rank: "king" ,
+suit: "hearts" ,
+cardImage: "images/king-of-hearts.png"},
+
+{rank: "king" ,
+suit: "diamonds" ,
+cardImage: "images/king-of-diamonds.png"}
+];
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardsInPlay = [];
 
 
+var checkForMatch = function()
+{
+
+if (cardsInPlay[0] === cardsInPlay[1])
+	{alert("You found a match!");}
+ else
+ 	alert("Sorry, try again.");
+}
+
+var flipCard = function (cardId){
+
+console.log("User flipped " + cards[cardId].rank );
+
+console.log(cards[cardId].cardImage );
+
+console.log(cards[cardId].suit );
+
+cardsInPlay.push(cards[cardId].rank);
+
+
+if (cardsInPlay.length === 2)
+{
+	checkForMatch();
+}
+
+}
+
+flipCard(0);
+flipCard(2);
 
